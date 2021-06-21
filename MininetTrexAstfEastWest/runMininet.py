ccb00c16ab09700a332fa6bf4d860271559cd264
root <bidhovbizar@gmail.com>
2021-06-22 02:46:13 +0530

    ed

---
+++ MininetTrexAstfEastWest/runMininet.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python                                                                            
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+
+from mininet.cli import CLI
+import subprocess
+import time
+
+def simpleTest():
+    "Create and test a simple network"
+    net = Mininet()
+    c1 = net.addController()
+    s1 = net.addSwitch('s1')
+    h1 = net.addHost('h1', ip='10.0.0.1')
+    net.addLink(h1, s1)
+    h2 = net.addHost('h2', ip='10.0.0.2')
+    net.addLink(h2, s1)
+    h3 = net.addHost('h3', ip='10.0.0.3')
+    net.addLink(h3, s1)
+    net.start()
+
+    print("Setting up TRex1 on H1")
+    h1.cmd("./setupH1MininetPart1.sh")
+    print("Setuped Part1: Links and connections")
+    subprocess.call("pwd", shell=True)
+    subprocess.call("./setupTrexH1Link.sh", shell=True)
+    print("\nSetuped Rootnamespace Links to Join to bridge")
+    h1.cmd("./setupH1MininetPart2.sh")
+    print("Setuped Part2: The server and client is up and running")
+
+    print("Setting up TRex2 on H2")
+    h2.cmd("./setupH2MininetPart1.sh")
+    print("Setuped Part1: Links and connections")
+    subprocess.call("pwd", shell=True)
+    subprocess.call("./setupTrexH2Link.sh", shell=True)
+    print("\nSetuped Rootnamespace Links to Join to bridge")
+    h2.cmd("./setupH2MininetPart2.sh")
+    print("Setuped Part2: The server and client is up and running")
+
+    #Start the tcpdump for host h1,h2 and h3
+    print("\nStarting tcpdump on all the host")
+    h1.cmd("xterm -hold -T 'tcpdump H1 veth0' -e tcpdump -nli veth0 &", shell=True)
+    h1.cmd("xterm -hold -T 'tcpdump H1 veth1' -e tcpdump -nli veth1 &", shell=True)
+    h2.cmd("xterm -hold -T 'tcpdump H2 veth2' -e tcpdump -nli veth2 &", shell=True)
+    h2.cmd("xterm -hold -T 'tcpdump H2 veth3' -e tcpdump -nli veth3 &", shell=True)
+    
+    print( "Dumping host connections" )
+    dumpNodeConnections(net.hosts)
+    print( "Testing network connectivity" )
+    net.pingAll()
+    
+    #h1.cmd('bash ./setupH1Mininet.sh')
+    print( "Starting Commandline Interface")
+    CLI(net)
+
+    net.stop()
+
+if __name__ == '__main__':
+    # Tell mininet to print useful information
+    setLogLevel('info')
+    simpleTest()
---
+++ MininetTrexAstfEastWest/setupH1MininetPart1.sh
@@ -0,0 +1,38 @@
+#Run the following script to install TRex2 in host1 
+#        (Trex1)
+#          |
+# (h1,Trex2) -- (s1) -- (h2)
+#
+#			Part 1
+#Connect 2 veth pair to switch s1 in mininet
+ip link add veth0 type veth peer name eth0
+ip link add veth1 type veth peer name eth1
+ip link set eth0 netns 1
+ip link set eth1 netns 1
+ovs-vsctl add-port s1 eth0
+ovs-vsctl add-port s1 eth1
+ip address add 10.0.0.10/24 dev veth0
+ip address add 10.0.0.11/24 dev veth1
+ip link set veth0 up
+ip link set veth1 up
+
+#			End of Part 1 
+#eth2 and eth3 are moved to root namespace so run this from there and finish the rest
+#ip link set eth0 up
+#ip link set eth1 up
+
+#			Part 2
+#ip address show veth0 | grep inet
+#ip address show veth1 | grep inet
+#ping 10.0.0.2 -c 1
+#ping 10.0.0.3 -c 1
+#echo ""
+#ovs-vsctl show
+#echo ""
+#cd ./trex1/v2.89/
+##Run the Trex Server in on xterm and Trex console/client in another console
+#xterm -T "h1 Server" -e ./t-rex-64 --cfg /etc/trex_cfg1.yaml -i &
+#sleep 10
+##xterm -hold -T "h1 Client/console" -e ./trex-console -f -s 10.0.0.1 -p 4601 
+#echo
+#echo "The TRex2 will be deleted when Mininet closes"
